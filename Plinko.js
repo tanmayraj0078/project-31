a8cf2ad0ad2ca6490283ffba3b0586fa9381aa4c
tanmayraj0078 <88242699+tanmayraj0078@users.noreply.github.com>
2021-08-12 18:42:17 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinko{
+    constructor(x,y){
+      var options = {
+         isStatic: true
+      }
+      this.body = Bodies.circle(x,y,15,options);
+      this.radius = this.body.circleRadius;
+      World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        fill("white")
+        ellipseMode(CENTER)
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+    }
+}
